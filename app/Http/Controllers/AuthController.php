9bb3f84748d150cbc39a0d40be443864c1787c5a
shemaynursidivav <nursidivashimey@gmail.com>
2026-07-13 11:26:31 +0700

    Tambah header no-cache di halaman login untuk cegah error 419

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,11 @@ class AuthController extends Controller
 {
     public function showLogin()
     {
-        return view('login');
+        return response()
+            ->view('login')
+            ->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+            ->header('Pragma', 'no-cache')
+            ->header('Expires', '0');
     }
 
     public function login(Request $request)
